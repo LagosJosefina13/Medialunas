58e73861a3c2499a9e0c70fb961012558cd05e8c
josefina-lagos <josefinalagos289@gmail.com>
2023-07-07 11:22:34 -0300

    En el header.html creamos el menu, agregamos las rutas y los links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./componentes/header/header.component";
+import { ItemsComponent } from './componentes/items/items.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
