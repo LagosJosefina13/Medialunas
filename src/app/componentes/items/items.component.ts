8110e53b6dfe173e9839f1b6ef1345015382912f
josefina-lagos <josefinalagos289@gmail.com>
2023-07-07 10:29:22 -0300

    En el ts de item importamos el item y luego creamos arreglos

--- src/app/componentes/items/items.component.ts
+++ src/app/componentes/items/items.component.ts
@@ -1,14 +1,29 @@
 import{Component, OnInit } from '@angular/core';
-import {Item} from '../models/item'
+import {Item} from '../models/item'; //creamos un import llamado Item
 @Component({
     selector: 'app-items',
     templateUrl: './items.component.html',
     styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit{
-items: Item[ ];
+items: Item []=[];
 constructor() {}
 ngOnInit(): void {
-    
+    this.items= [
+        {
+            id: 0,
+            title: 'Medialunas',
+            price: 10.5,
+            quantity: 4,
+            completed: false
+        },
+        {
+            id: 0,
+            title: 'Tarta Toffe',
+            price: 3.5,
+            quantity: 8,
+            completed: true
+        }
+    ]
 }
 }
